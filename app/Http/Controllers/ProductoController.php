97f388a3cca6d6b580a1e647855f8e1fad742e9d
Robin Castillo <robin.castillo@catolica.edu.sv>
2024-10-16 11:32:37 -0600

    iniciando repositorio

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $productos = Producto::all();
+        return view('producto.index', compact('productos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+       // $proveedores = Proveedor::all();
+        return view('producto.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $producto = new Producto();
+
+        $producto->Nombre = $request->get('nombre');
+        $producto->Descripcion = $request->get('descripcion');
+        $producto->Categoria = $request->get('categoria');
+        $producto->Proveedor = $request->get('proveedor');
+        $producto->Precio = $request->get('precio');
+        $producto->Cantidad = $request->get('cantidad');
+        $producto->Unidad_medida = $request->get('unidad');
+        
+        $producto->save();
+
+        $productos = Producto::all();
+        return view('producto.index', compact('productos'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Producto $producto)
+    {
+        Producto::find($id)->delete();
+        return redirect()->route('indexpr');
+    }
+}
---
+++ database/migrations/2024_10_16_172259_create_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre')->nullable();
+            $table->string('Descripcion')->nullable();
+            $table->string('Categoria')->nullable();
+            $table->string('Proveedor')->nullable();
+            $table->double('Precio')->nullable();
+            $table->integer('Cantidad')->nullable();
+            $table->integer('Unidad_medida')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
