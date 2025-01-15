a7c7e526d100ed867bc65a1d834407ed226abb68
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-01-15 09:54:46 -0600

    cambios en cotidetalle

---
+++ app/Http/Controllers/CotidetalleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cotidetalle;
+use Illuminate\Http\Request;
+
+class CotidetalleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cotidetalle $cotidetalle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cotidetalle $cotidetalle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cotidetalle $cotidetalle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cotidetalle $cotidetalle)
+    {
+        //
+    }
+}
